6f710f548ca586c5e7cdf915b76d34f1c3a1f92e
Martin Cook <33993749+martycook@users.noreply.github.com>
2022-11-28 07:38:44 -0600

    adding implementation

---
+++ src/Services/CG.Purple.Host/Pages/Logs/Index.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace CG.Purple.Host.Pages.Logs;
+
+/// <summary>
+/// This class is the code-behind for the <see cref="Index"/> page.
+/// </summary>
+public partial class Index
+{
+    // *******************************************************************
+    // Fields.
+    // *******************************************************************
+
+    #region Fields
+
+    /// <summary>
+    /// This field contains a reference to breadcrumbs for the view.
+    /// </summary>
+    protected readonly List<BreadcrumbItem> _crumbs = new()
+    {
+        new BreadcrumbItem("Home", href: "/"),
+        new BreadcrumbItem("Logs", href: "/logs")
+    };
+
+    #endregion
+}
